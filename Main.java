5d72029c183927a8a66e94dcea50463f1b3aff5f
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2025-01-06 19:13:41 +0000

    aa

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ public class Main
 {
   public static void main(String[] args)
   {
+    Time test = new Time(1, 2, 3);
+    System.out.println(test);
     // Test your tick method below using the Scanner class to
     // prompt the user for an initial time, and use the tick method
     // on that Time object.  Check that the resulting time is correct
--- Time.java
+++ Time.java
@@ -2,20 +2,29 @@ public class Time
 {
   // TODO: Part A - put your attributes here
 
-
+  private int hours;
+  private int minutes;
+  private int seconds; 
   // TODO: Part B - create your non-default constructor
+  public Time(int h, int m, int s) {
+    this.hours = h;
+    this.minutes = m;
+    this.seconds = s;
+  }
   /**
   * Precondition: The number of hours is between 0 and 23 inclusive.
   * The number of minutes and seconds is between 0 and 59 inclusive.
   */
-
-
   // TODO: Part C - create your default constructor
 
-
+  public Time() {
+    this(0, 0, 0); // 12 AM MIDNIGHT
+  }
   // TODO: Part D - complete the documentation for the pad method
   /**
-  * Description: 
+  * Description: Method pad takes in an integer value. If value is less than 10, meaning it is not
+  * double digit, it will take a single digit number and add a zero in front of it. If value is greater than 10
+  * it will just turn it into a string anyways without a zero in front.
   * Precondition: The value is greater than 0.
   */
   private String pad(int value)
